016b0f8c1f72e7d76446a795e8ba246e0f5d77bc
mehdi gharibi <mgharibiwork@gmail.com>
2023-06-23 21:23:20 +0330

    add Colors to Constants

---
+++ lib/constants/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const Color tdRed = Color(0xffDA4040);
+const Color tdBlue = Color(0xff5d52ee);
+const Color tdBlack = Color(0xff3a3a3a);
+const Color tdGrey = Color(0xff717171);
+const Color TdBGcolor = Color(0xffeeeff5);
